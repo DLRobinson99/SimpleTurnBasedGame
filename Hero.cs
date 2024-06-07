150c4a09c536fb35dd7728eec2533110dd3221f2
User-Admin <drobinson51@qub.ac.uk>
2024-06-07 14:18:38 +0100

    Add project files.

---
+++ Hero.cs
@@ -0,0 +1,83 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SimpleTurnBasedGame
+{
+    internal class Hero
+    {
+        private int maxHealth;
+        private int currentHealth;
+        private int attackRating;
+        private int healingRating;
+        private string heroName;
+        private Random randomValue;
+
+        public int Health { get {return currentHealth;} }
+
+        public string HeroName { get { return heroName; } }
+
+        public bool Defeated { get { return currentHealth <= 0; } }
+
+
+
+
+        public Hero(int maxHealth, int attackRating, int healingRating, string heroName)
+        {
+            this.maxHealth = maxHealth;
+            this.currentHealth = maxHealth;
+            this.attackRating = attackRating;
+            this.healingRating = healingRating;
+            this.heroName = heroName;
+            this.randomValue = new Random();
+        }
+
+        public void Battle(Hero heroToAttack)
+
+        {
+
+            double rng = randomValue.NextDouble();
+
+            rng = rng / 2 + 0.5f;
+
+            int damageDealt = (int)(attackRating * rng);
+
+            heroToAttack.damageReceived(damageDealt);
+
+            Console.WriteLine(heroName + " targeted " + heroToAttack.heroName + " and dealt " + damageDealt + " damage ");
+
+
+        }
+
+
+
+        public void damageReceived(int damageDealt)
+        {
+
+            currentHealth -= damageDealt;
+
+            if (Defeated)
+            {
+                Console.WriteLine(HeroName + " has been defeated.");
+            }
+
+        }
+
+        public void Heal()
+
+        {
+
+            double rng = randomValue.NextDouble();
+
+            rng = rng / 2 + 0.25f;
+
+            int heal = (int)(healingRating * rng);
+
+            currentHealth = heal + currentHealth > maxHealth ? maxHealth : currentHealth + heal;
+
+            Console.WriteLine(HeroName + " heals " + heal);
+
+
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using SimpleTurnBasedGame;
+using System;
+
+Hero player = new Hero(110, 29, 12, "Player");
+Hero adversary = new Hero(75, 8, 4, "Enemy Goblin");
+Random random = new Random();
+
+
+while (!player.Defeated && !adversary.Defeated) { 
+Console.WriteLine(player.HeroName + " HP = " + player.Health + ". " + adversary.HeroName + "HP = " + adversary.Health);
+Console.WriteLine("Take an action");
+
+string action = Console.ReadLine();
+
+if (action == "a")
+{
+
+    player.Battle(adversary);
+}
+else
+    player.Heal();
+
+if (player.Defeated || adversary.Defeated)
+    {
+        break;
+    }
+
+Console.WriteLine(player.HeroName + " HP = " + player.Health + ". " + adversary.HeroName + "HP = " + adversary.Health);
+
+Console.WriteLine("Enemy turn!");
+
+int rand = random.Next(0, 2);
+
+if (rand == 0)
+{
+   adversary.Battle(player);
+} else
+    adversary.Heal();
+}
